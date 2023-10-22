50022245fbe1fcfdb26b459619348180bef03ca5
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2023-10-21 22:50:53 -0300

    Props children

---
+++ pages/basicos/componentesComFilhos.jsx
@@ -0,0 +1,13 @@
+import Lista from "../../components/Lista";
+import Item from "../../components/Item";
+export default function compoenteComFilhos() {
+  return (
+    <div>
+      <Lista>
+        <Item content="Item #11"/>
+        <Item content="Item #12"/>
+        <Item content="Item #13"/>
+        </Lista>
+    </div>
+  )
+}
